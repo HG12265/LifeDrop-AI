f6e637389c662e93e4bebd2ad0a692e256654d60
HG12265 <gowtham114411@gmail.com>
2026-03-01 19:20:25 +0530

    network issue

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,10 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
     origin: [
     "http://localhost:5173",
-    "https://life-drop-ai.vercel.app"
+    "https://life-drop-ai.vercel.app",
+    "capacitor://localhost", 
+    "https://localhost",
+    "http://localhost"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
